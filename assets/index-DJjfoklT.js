df3f074a26c85e75864181b6ddba26453b20d93d
Jad Haddad <jadkh6@gmail.com>
2026-04-07 18:06:11 -0500

    Update index-DJjfoklT.js

--- assets/index-DJjfoklT.js
+++ assets/index-DJjfoklT.js
@@ -22817,7 +22817,7 @@ function tO() {
 function nO() {
 	return S.jsxs(PS, {
 		children: [S.jsx(hm, {
-			path: "/jad-haddad-site/",
+			path: "jad-jkh.github.io",
 			component: tO
 		}), S.jsx(hm, {
 			component: oC
